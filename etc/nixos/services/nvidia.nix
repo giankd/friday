fa22d243495feb65472f317d7e7b5489b9c3c0b0
giankd <giankd@gmail.com>
2023-08-05 23:05:52 +0200

    feat(gpu): properly set NVIDIA drivers, CUDA support and blender override

--- etc/nixos/services/nvidia.nix
+++ etc/nixos/services/nvidia.nix
@@ -13,7 +13,24 @@
     builtins.elem (lib.getName pkg) [
       "nvidia-x11"
       "nvidia-settings"
+      "nvidia-persistenced"
     ];
+
+  environment.systemPackages = with pkgs; [
+      pciutils
+      cudatoolkit
+  ];
+
+  systemd.services.nvidia-control-devices = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.ExecStart = "${pkgs.linuxPackages.nvidia_x11.bin}/bin/nvidia-smi";
+  };
+
+  nixpkgs.overlays = [
+      (final: prev: {
+       blender = prev.blender.override { cudaSupport = true; };
+       })
+  ];
 
   # Tell Xorg to use the nvidia driver
   services.xserver.videoDrivers = ["nvidia"];
@@ -23,9 +40,8 @@
     # Modesetting is needed for most wayland compositors
     modesetting.enable = true;
 
-    # Use the open source version of the kernel module
-    # Only available on driver 515.43.04+
-    open = true;
+    # https://discourse.nixos.org/t/cant-use-nvidia-prime-with-laptop/6767/8
+    nvidiaPersistenced = true;
 
     # Enable the nvidia settings menu
     nvidiaSettings = true;
@@ -33,6 +49,7 @@
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
 
+    # https://nixos.wiki/wiki/Nvidia
     prime = {
         offload = {
             enable = true;
