1fb6a1589ec0c43a788acc4217daf723ac3805e0
giankd <giankd@gmail.com>
2023-08-29 11:38:30 +0200

    feat: multiscreen support

---
+++ home/.screenlayout/big_screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-2 --mode 3840x2160 --pos 0x0 --rotate normal --scale 0.34 --output DP-1 --off --output HDMI-1 --off
